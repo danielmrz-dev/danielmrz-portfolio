04b523181fea0a31273af1f1640f6f00dc3800a8
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-06 18:06:56 -0300

    Adiciona botões de mudar idioma no header

--- src/app/components/header-footer/header-footer.component.ts
+++ src/app/components/header-footer/header-footer.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { ISocialMediaLink } from '../../models/social-media-link.interface';
+import { Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ISocialMediaLink } from '../../models/social-media-link.interface';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header, app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule],
   templateUrl: './header-footer.component.html',
   styleUrl: './header-footer.component.scss'
 })
-export class HeaderFooterComponent {
+export class HeaderFooterComponent implements OnInit {
   socialMediaLinks: ISocialMediaLink[] = [
     {
       socialMedia: 'Visite o meu perfil no Github',
@@ -31,5 +32,20 @@ export class HeaderFooterComponent {
       icon: 'icon-x-twitter.svg',
       link: 'https://x.com/danielmrz_dev',
     },
-  ]
+  ];
+
+  @HostBinding('class.is-header') isHeader = false;
+  @HostBinding('class.is-footer') isFooter = false;
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    const tagName = this.el.nativeElement.tagName.toLowerCase();
+
+    if (tagName === 'app-header') {
+      this.isHeader = true;
+    } else if (tagName === 'app-footer') {
+      this.isFooter = true;
+    }
+  }
 }
