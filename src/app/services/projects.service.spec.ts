4e91f88e5bdba71196dab8be9493ada6aaa57315
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-04-22 12:21:28 -0300

    Testa service ProjectsService

--- src/app/services/projects.service.spec.ts
+++ src/app/services/projects.service.spec.ts
@@ -1,12 +1,34 @@
 import { TestBed } from "@angular/core/testing";
 import { ProjectsService } from "./projects.service";
-import { provideHttpClient } from "@angular/common/http";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { HttpErrorResponse, provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { ProjectsList } from "../models/projects-list.type";
+
+const mockProjetos: ProjectsList = [
+    {
+        images: {
+            small: "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/blitjo9cbnmtbaybeiys.jpg",
+            large: "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/blitjo9cbnmtbaybeiys.jpg"
+        },
+        name: "Designo Multi-Page Website",
+        technologies: [
+            "angular",
+            "angular material",
+            "sass",
+            "typescript",
+            "rxjs"
+        ],
+        repository: "https://github.com/danielmrz-dev/designo-website",
+        liveSite: "https://designo-website-jet.vercel.app"
+    },
+]
+const apiLink = "https://danielmrz-portfolio-backend-production.up.railway.app/projetos";
 
 describe(ProjectsService.name, () => {
 
     let service: ProjectsService;
     let prot = ProjectsService.prototype;
+    let httpController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -17,23 +39,58 @@ describe(ProjectsService.name, () => {
             ]
         })
         service = TestBed.inject(ProjectsService);
+        httpController = TestBed.inject(HttpTestingController)
+    })
+
+    afterEach(() => {
+        httpController.verify();
     })
 
     it('should create', () => {
         expect(service).not.toBeNull();
     });
 
-    describe(`Method => ${prot.getProjects.name}()`, () => {
+    describe(`#${prot.getProjects.name}()`, () => {
         it('should return the list of projects', () => {
-            expect(true).toBeTruthy();
+            service.getProjects().subscribe((projetos) => {
+                expect(projetos).toEqual(mockProjetos);
+            });
+            const req = httpController.expectOne(apiLink);
+            expect(req.request.method).toBe("GET");
+            req.flush(mockProjetos);
         });
-    });
 
-    describe(`Properties`, () => {
-        it('should initialize with first value', () => {
-            expect(service.api).toBe('https://danielmrz-portfolio-backend-production.up.railway.app/projetos');
+        it('should perform a http request every time the method is called', () => {
+            service.getProjects().subscribe();
+            service.getProjects().subscribe();
+            const reqs = httpController.match(apiLink);
+            expect(reqs.length).toBe(2);
         });
-    });
-    
 
+        it('should return an empty list when there is no projects on the list', () => {
+            service.getProjects().subscribe((projetos) => {
+                expect(projetos.length).toBe(0);
+            })
+            const req = httpController.expectOne(apiLink);
+            req.flush([]);
+        });
+
+        it('should return an error when the request fails', () => {
+            service.getProjects().subscribe({
+                next: () => {
+                    fail("Falhando propositamnete para efeitos de teste.");
+                },
+                error: (error: HttpErrorResponse) => {
+                    expect(error.status).toBe(422);
+                }
+            })
+
+            const req = httpController.expectOne(apiLink);
+            req.flush("Erro", { status: 422, statusText: "Erro simulado" });
+        });
+        
+        
+        
+        
+    });
 });
