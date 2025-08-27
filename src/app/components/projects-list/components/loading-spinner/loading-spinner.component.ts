4669a00396515c4e0707400be5fe3c3f3a026194
Daniel Mariz <danielmrz.dev@gmail.com>
2025-08-27 16:35:15 -0300

    Adiciona link externo para projetos

--- src/app/components/projects-list/components/loading-spinner/loading-spinner.component.ts
+++ src/app/components/projects-list/components/loading-spinner/loading-spinner.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, Input } from '@angular/core';
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject, Input } from '@angular/core';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { ProjectsList } from '../../../../models/projects-list.type';
 import { Language } from '../../../../models/language.type';
@@ -20,6 +20,7 @@ export class LoadingSpinnerComponent {
   private readonly spinner = inject(NgxSpinnerService);
   readonly _translationsService = inject(TranslationsService);
   currentLanguage: Language = 'en';
+  portfolioLink = 'https://www.frontendmentor.io/profile/danielmrz-dev/solutions';
 
   ngOnInit(): void {
     this.spinner.show();
@@ -33,8 +34,25 @@ export class LoadingSpinnerComponent {
       "pt": "Nosso servidor tava no modo soneca. Estamos cutucando ele pra acordar! Por favor, aguarde.",
       "en": "Our server was in snooze mode. We’re poking it to wake up! Please, wait.",
       "es": "Nuestro servidor estaba en modo siesta. ¡Lo estamos pinchando para que despierte! Un momento, por favor.",
-    }
-
+    };
     return text[lang];
+  }
+
+  getMaintenanceTexts(lang: Language): string {
+    const text: { [key in Language]: string } = {
+      "pt": "Esta parte do meu portfolio está em manutenção. Para ver meus projetos, por favor acesse o link abaixo.",
+      "en": "This section of my portfolio is under maintenance. To view my projects, please visit the link below.",
+      "es": "Esta parte de mi portafolio está en mantenimiento. Para ver mis proyectos, por favor accede al enlace abajo.",
+    };
+    return text[lang];
+  }
+
+  getLinkText(lang: Language): string {
+    const text: { [key in Language]: string } = {
+      "pt": "Link para meus projetos",
+      "en": "Link to my projects",
+      "es": "Enlace para mis projectos",
+    };
+    return `${text[lang]} ↗️`;
   }
 }
